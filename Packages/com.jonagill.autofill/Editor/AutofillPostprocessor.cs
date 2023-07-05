37de36ff49cdf5bdd1dd959dab000a6297f0e7c0
Jon Gill <jonagill@gmail.com>
2023-07-04 17:24:38 -0700

    Tentative change to defer running autofill when an asset is imported during a compilation

--- Packages/com.jonagill.autofill/Editor/AutofillPostprocessor.cs
+++ Packages/com.jonagill.autofill/Editor/AutofillPostprocessor.cs
@@ -1,17 +1,81 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 namespace Autofill.Editor
 {
+    [InitializeOnLoad]
     public class AutofillPostprocessor : AssetPostprocessor
     {
-        public static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+        private const string DEFERRED_PATHS_STATE_KEY = "AutofillPostProcessorDeferredPaths";
+        private const char DEFERRED_PATHS_SEPARATOR = ';';
+
+        private static readonly List<string> assetPathsForDeferredUpdate = new();
+        
+        static AutofillPostprocessor()
+        {
+            AssemblyReloadEvents.beforeAssemblyReload += BeforeAssemblyReload;
+
+            if (!EditorApplication.isCompiling)
+            {
+                ProcessDeferredUpdates();
+            }
+        }
+
+        public static void OnPostprocessAllAssets(
+            string[] importedAssets, 
+            string[] deletedAssets, 
+            string[] movedAssets,
             string[] movedFromAssetPaths)
         {
             foreach (var assetPath in importedAssets)
             {
                 if (assetPath.EndsWith(".prefab"))
+                {
+                    if (EditorApplication.isCompiling)
+                    {
+                        if (!assetPathsForDeferredUpdate.Contains(assetPath))
+                        {
+                            assetPathsForDeferredUpdate.Add(assetPath);
+                        }
+                    }
+                    else
+                    {
+                        var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+                        if (prefab != null)
+                        {
+                            AutofillEditorUpdater.UpdateAndSavePrefab(prefab);
+                        }    
+                    }
+                }
+            }
+        }
+        
+        private static void BeforeAssemblyReload()
+        {
+            // Save any deferred paths to SessionState
+            if (assetPathsForDeferredUpdate.Count > 0)
+            {
+                var prevPaths = SessionState.GetString(DEFERRED_PATHS_STATE_KEY, "");
+                if (!string.IsNullOrEmpty(prevPaths))
+                {
+                    assetPathsForDeferredUpdate.Add(prevPaths);
+                }
+
+                var joinedPaths = string.Join(DEFERRED_PATHS_SEPARATOR, assetPathsForDeferredUpdate);
+                SessionState.SetString(DEFERRED_PATHS_STATE_KEY, joinedPaths);
+            }
+        }
+
+        private static void ProcessDeferredUpdates()
+        {
+            var deferredPaths = 
+                SessionState.GetString(DEFERRED_PATHS_STATE_KEY, "")
+                .Split(DEFERRED_PATHS_SEPARATOR);
+            
+            foreach (var assetPath in deferredPaths)
+            {
+                if (!string.IsNullOrEmpty(assetPath))
                 {
                     var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
                     if (prefab != null)
@@ -20,6 +84,9 @@ namespace Autofill.Editor
                     }
                 }
             }
+            
+            // Clear our previous state
+            SessionState.EraseString(DEFERRED_PATHS_STATE_KEY);
         }
     }
 }
